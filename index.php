788e1b76d33f6067516f6b1200b5c904e635dd7e
Patrik Johansson <patrik@ptjwebben.se>
2015-12-05 14:18:40 +0100

    Now includes an internal API (new dependency) and also requires update of the PHP-Snippets dependency The internal "API" is YouTube-Downloader by John Eckman

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+<?php include('PHP-Snippets/include_functions.php'); ?>
 <html>
 	<head>
 		<title>Youtube->Emby File Generator</title>
@@ -20,7 +21,24 @@
 		<form method="POST" action="download.php" target="log">
 			<input type="text" name="ch" placeholder="Youtube username">
 			<!-- The API should take a youtube video id (?v=YOUTUBE_VIDEO_ID) and redirect to a directlink -->
-			<input type="text" name="api_url" placeholder="Youtube directlink API" value="http://popeen.com/api/youtube/direct?v=">
+			<?php
+				
+				/**
+				
+				If you for some reason don't want to use the internal API an easy 
+				way of finding external APIs is by going to this google search
+				https://www.google.com/search?q=youtube+downloader+Put+in+just+the+ID+bit,+the+part+after+v%3D.
+				and add getvideo.php?videoid= to the url. Make sure to test that it
+				works before using it. Still its recommended to use the internal API
+				because then you are the only one that controlls if the API stays up.
+				
+				**/
+			
+			?>
+			<select name="api_url">
+				<option value="<?php echo get_url_directory() .'YouTube-Downloader/getvideo.php?videoid='; ?>">Internal API (Recommended)</option>
+				<option value="http://popeen.com/api/youtube/direct?v=">popeen.com</option>
+			</select>
 			<input type="checkbox" name="reset" id="reset" value="1" checked="checked"> <label for="reset">Reset</label>
 			<input type="submit" value="Generate Emby files">
 		</form>
@@ -28,7 +46,9 @@
 		<span class="os">
 			Open Source under the 
 			<a href="https://github.com/Kakadua/Youtube-Emby-generator/blob/master/LICENSE">MIT License</a>. 
-			Get it at <a href="https://github.com/Kakadua/Youtube-Emby-generator">GitHub</a>
+			Get it at <a href="https://github.com/Kakadua/Youtube-Emby-generator">GitHub</a>.
+			The API is under <a href="http://www.gnu.org/licenses/old-licenses/gpl-2.0.html">GPL</a>
+			and also availiable on <a href="https://github.com/jeckman/YouTube-Downloader">GitHub</a>.
 		</span>
 	</body>
 </html>
